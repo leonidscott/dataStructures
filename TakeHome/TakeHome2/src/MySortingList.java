0732074df5e7e8bf2d7adb1619de6095255ed8ca
Lenny Scott <scot0530@morris.umn.edu>
2014-11-02 02:40:46 -0600

    added tests to takeHome 2

---
+++ TakeHome/TakeHome2/src/MySortingList.java
@@ -0,0 +1,61 @@
+import java.util.Iterator;
+
+
+public class MySortingList<E extends Comparable<E>> implements SortingList<E> {
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int uniqueValueCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void add(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int frequencyOf(E item) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E get(int index) throws ListIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(int index) throws ListIndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
